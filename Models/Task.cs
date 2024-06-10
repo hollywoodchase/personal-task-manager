9e74cd5b33ee9b1a4898c5a8b49e0823d2a90b2c
Danny Calise <32149505+hollywoodchase@users.noreply.github.com>
2024-06-10 09:13:15 -0400

    Create Task.cs

---
+++ Models/Task.cs
@@ -0,0 +1,12 @@
+// Models/Task.cs
+namespace PersonalTaskManager.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Status { get; set; }
+    }
+}
